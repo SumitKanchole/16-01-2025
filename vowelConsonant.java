033920d9a1eeedcd895107506d8613c12b56df6f
Sumit Kanchole <kancholesumit@gmail.com>
2025-01-17 14:23:54 +0530

    Create vowelConsonant.java

---
+++ vowelConsonant.java
@@ -0,0 +1,28 @@
+package s1;
+import java.util.*;
+public class vowelConsonant {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the word to check how many Consonants are present that word : ");
+		String str = sc.nextLine();
+		int vowels=0;
+		int consonants=0;
+		
+		for(int i=0; i<str.length();i++) {
+			if(str.charAt(i)==' ') {
+				continue;
+			}
+			else if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u') {
+				vowels++;
+			}
+			else {
+				consonants++;
+			}
+		}
+		System.out.println("there are "+vowels+" vowels are here!");
+		System.out.println("there are "+consonants+" consonants are here!");
+
+	}
+
+}
